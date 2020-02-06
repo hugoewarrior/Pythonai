20a472c7b1f909910991ced56a83670aeb3073bc
Hugo Guerrero <46351590+hugoewarrior@users.noreply.github.com>
2020-02-06 20:58:42 +0000

    Update index.py

--- index.py
+++ index.py
@@ -41,14 +41,33 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data,
                                                        padding="post",
                                                        maxlen=256)
 # Modelo
-vocabSize = 1000
+vocab_size = 10000
 
 model = keras.Sequential()
-model.add(keras.layers.Embedding(vocabSize, 16))
+model.add(keras.layers.Embedding(vocab_size, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation=tf.nn.relu))
-model.add(keras.layers.Dense(1, activation=tf.nn.selu))
+model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
 
-model.summary()
+# model.summary()
 
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc"])
+model.compile(optimizer="adam",
+              loss="binary_crossentropy",
+              metrics=["acc"])
+
+x_val = train_data[:1000]
+partial_x_train = train_data[1000:]
+
+y_val = train_labels[:1000]
+partial_y_train = train_labels[1000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+results = model.evaluate(test_data, test_label)
+
+print(results)
